cc8fde48ba07553f8052aa59e3b0708a0c18cd69
Mottaqui Karim <taqqui.karim@gmail.com>
2015-11-16 23:58:07 -0500

    added homework

---
+++ javascript/app.js
@@ -0,0 +1,23 @@
+var player1;
+var player2;
+
+// TO TEST, SET VALUES OF PLAYER HERE
+player1 = "rock"; // you can test your code by changing this to "paper" or "scissor"
+player2 = "paper"; // you can test your code by changing this to "rock" or "scissor"
+
+var winner;
+if ( player1 === "rock" && player2 === "paper" ) {
+    winner = "player2";
+}
+// write all the other player1 and player2 possibilities here
+// OHTER IF STATEMENTS HERE
+
+
+// END
+
+console.log( winner );
+
+// FOOD FOR THOUGHT:
+// how annoying is it that you have to go back and change your variables
+// to see different outcomes??
+// what could you do to the code to ensure that you could run our logic over and over?
