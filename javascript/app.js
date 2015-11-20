600697efbf2416d2ceee507184828dadc6124048
Kelsey Stahl <kstahlbrew@gmail.com>
2015-11-20 18:26:45 -0500

    attempted RPS arguments

--- javascript/app.js
+++ javascript/app.js
@@ -5,9 +5,28 @@ var player2;
 player1 = "rock"; // you can test your code by changing this to "paper" or "scissor"
 player2 = "paper"; // you can test your code by changing this to "rock" or "scissor"
 
-var winner;
-if ( player1 === "rock" && player2 === "paper" ) {
-    winner = "player2";
+function getWinner( player1, player2 ) {
+
+	var winner = 
+	if ( player1 === "rock" && player2 === "paper" ) {
+	    winner = "player2"; 
+	if ( player1 === "rock" && player2 === "scissor" ) {
+	    winner = "player1"; 
+	if ( player1 === "rock" && player2 === "rock" ) {
+	    winner = "tie"; 
+	if ( player1 === "paper" && player2 === "rock" ) {
+	    winner = "player1"; 
+	if ( player1 === "paper" && player2 === "scissor" ) {
+	    winner = "player2"; 
+	if ( player1 === "paper" && player2 === "paper" ) {
+	    winner = "tie"; 
+	if ( player1 === "scissor" && player2 === "paper" ) {
+	    winner = "player1"; 
+	if ( player1 === "scissor" && player2 === "rock" ) {
+	    winner = "player2"; 
+	if ( player1 === "scissor" && player2 === "scissor" ) {
+	    winner = "tie"; 
+	return winner;
 }
 // write all the other player1 and player2 possibilities here
 // OHTER IF STATEMENTS HERE
@@ -15,7 +34,8 @@ if ( player1 === "rock" && player2 === "paper" ) {
 
 // END
 
-console.log( winner );
+var final = getWinner( "paper", "rock" )
+console.log( final);
 
 // FOOD FOR THOUGHT:
 // how annoying is it that you have to go back and change your variables
